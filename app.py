aa34cee0b9d8318dc2ec9437e7c365d9b44c47ca
TW168 <tony.wei@outlook.com>
2024-03-07 16:13:10 -0600

    Make more correction in lat_lng

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from urllib.parse import urlencode
 import sqlite3
 import re
 import streamlit as st 
-from utils.process_excel_data import process_excel_data
+from utils.process_ipg_data import process_ipg_data
 
 
 
@@ -13,7 +13,7 @@ file_upload = st.file_uploader('Choose a Pickup Detail Report', key=['.xlsx'])
 if file_upload is None:
     st.write('No file is selected')
 else:
-    processed_df = process_excel_data(file_upload)
+    processed_df = process_ipg_data(file_upload)
     st.dataframe(processed_df)
     null_lat_lng = processed_df[processed_df['Latitude'].isnull()]
     st.write(null_lat_lng)
